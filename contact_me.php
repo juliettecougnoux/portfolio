7fc22d465c3729219985bb4f6dca5839281da7bc
juliette-cougnoux <jcougnoux@gmail.com>
2016-07-13 16:42:06 +0200

    Add files via upload
    modif contact_me.php

--- contact_me.php
+++ contact_me.php
@@ -19,8 +19,8 @@ $message = $_POST['message'];
 $to = 'jcougnoux@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Juliette cougnoux contact :  $name";
 $email_body = "Vous avez reçu un nouveau message de votre site.\n\n"."Voici les détails:\n\nNom: $name\n\nEmail: $email_address\n\nTéléphone: $phone\n\nMessage:\n$message";
-$headers = "From: flavien-medina@medinaflavien.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
+$headers = "De : juliettecougnoux@codecom.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Répondre à : $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
